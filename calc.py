efea7e5a06d435c6fcd25686cf79d1f9db6dd6c1
ZeBread <59803339+ZeBread@users.noreply.github.com>
2022-08-05 09:15:35 +0530

    Update and rename euclidean.py to calc.py

--- calc.py
+++ calc.py
@@ -2,5 +2,6 @@ a=[[0 for l in range(6)] for m in range(6)]
 for i in range(len(a)):
     for j in range(len(a[i])):
         a[i][j]=max(abs(j-0),abs(i-2))
+        #a[i][j]=abs(j-0)+abs(i-2)
         #print(a[i][j])
 print(a)
